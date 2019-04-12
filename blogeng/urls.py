86d88de1ea988d399fa5cf195e02d99593134e94
Vladislav Gorin <bkngg@yandex.ru>
2019-04-12 06:01:35 +0300

    Добавлено приложение blog, настроены некоторые html-элементы, подготовленна среда для дальнейшей разработки

--- blogeng/urls.py
+++ blogeng/urls.py
@@ -1,21 +1,25 @@
-"""blogeng URL Configuration
+"""
+Конфигурация Blogeng URL
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+Список `urlpatterns` направляет URL-адреса к представлениям. Для получения дополнительной информации, пожалуйста, смотрите:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Примеры:
+Функциональные представления
+    1. Добавить импорт: из представлений импорта my_app
+    2. Добавьте URL в urlpatterns: путь ('', views.home, name = 'home')
+Основанные на классе представления
+    1. Добавить импорт: из other_app.views import Home
+    2. Добавьте URL в urlpatterns: путь ('', Home.as_view (), name = 'home')
+Включая другой URLconf
+    1. Импортируйте функцию include (): из django.urls импортируйте include, путь
+    2. Добавьте URL в urlpatterns: path ('blog /', include ('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # обращайемся к файлу URLS в приложении BLOG
+    path('blog/', include('blog.urls')),
 ]
